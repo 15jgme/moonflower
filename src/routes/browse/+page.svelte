6da90bfdc0a7072fad9bf73787d1310af6df9635
Jackson Empey <47725944+15jgme@users.noreply.github.com>
2023-02-13 22:18:48 -0500

    Remove debug element from Browse

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -41,8 +41,7 @@
 
 <title>Browse</title>
 
-<h1 class="text-neutral-content p-5 font-bold text-xl">Browse recent work</h1>
-<p>{limitReached}</p>
+<h1 class="text-neutral-content p-5 font-bold text-xl">Browse recent work</h1>s
 <div class="flex-col justify-center space-y-3 px-5">
 	{#if data.user}
 		<div class="flex">
